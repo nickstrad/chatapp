10447398eb74d14563c7470ac9c94f47d33e9910
nickstrad <nickstrad.dev@gmail.com>
2019-07-24 16:48:03 -0500

    Changing starting port for backend serverj

--- backend/main.go
+++ backend/main.go
@@ -32,7 +32,7 @@ func main() {
   go r.run()
 
   //Start server
-  if err := http.ListenAndServe(":8080", nil); err != nil {
+  if err := http.ListenAndServe(":3001", nil); err != nil {
     log.Fatal("ListenAndServe:", err)
   }
 }
